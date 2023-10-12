47bd0ab2a4867da686c7e06dac4aee01fa073310
Frederico Thomaz dos Santos <fredericoufsj1@gmail.com>
2023-10-12 12:57:43 -0300

    apollo and graphql configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-function App() {
+import { gql, useQuery } from "@apollo/client";
 
-  return (
-    <h1>Hello World!!</h1>
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+interface Lesson{
+  id: string;
+  title: string;
+}
+
+function App() {
+  const{data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
+
+  return(
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
-export default App
+export default App;
